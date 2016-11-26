92fc8afe8fef308bcc6623ebf4ffbfbcc194cfe7
robert1826 <robert1826@gmail.com>
2016-11-26 17:46:18 +0200

    client send packet to server

--- client.py
+++ client.py
@@ -1,4 +1,11 @@
 import socket
+import signal
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 9000
+
+FILE_NAME = "some_file_name"
+TIMEOUT_VALUE = 3
 
 class packet:
 	def __init__(self, c=0, l=0, s=0, d=[]):
@@ -7,26 +14,46 @@ class packet:
 		self.seqno = s
 		self.data = d[:]
 
-	def toBuffer():
-		res = self.int_to_bytes(chksum, 16)
-		res += self.int_to_bytes(length, 16)
-		res += self.int_to_bytes(seqno, 32)
+	def toBuffer(self):
+		res = self.int_to_bytes(self.chksum, 16)
+		res += self.int_to_bytes(self.length, 16)
+		res += self.int_to_bytes(self.seqno, 32)
 		res += self.data
 		return res
 
-	def int_to_bytes(n, length):
+	def int_to_bytes(self, n, length):
 		res = str(n).encode()
 		while len(res) < length:
 			res = '0'.encode() + res
 		return res
 
+
+def signal_handler(signum, frame):
+	raise Exception("Timed out!")
+
+def request_file():
+	while True:
+		#signal.signal(signal.SIGALRM, signal_handler)
+		#signal.alarm(TIMEOUT_VALUE)
+
+		try:
+			p = packet(0, len(FILE_NAME), 0, FILE_NAME.encode())
+		   	sock.sendto(p.toBuffer(), (UDP_IP, UDP_PORT))
+
+			data, addr = sock.recvfrom(1024)
+
+
+			    # check if ACK
+			if len(data) == 8:
+					# turn the alarm off
+				break
+
+		except Exception, msg:
+			print "Timed out!"
+
+	# file name sent
+
 if __name__ == "__main__":
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    #sock.connect(("localhost", 9000))
-    data = "some data"
-    #send request here
-    sock.sendto(data, ("localhost",9000))
-    #loop receive	
-    #result = sock.recv(1024)
-    #print result
-    #sock.close()
+    request_file()
+    
--- server2.py
+++ server2.py
@@ -1,5 +1,12 @@
 import socket
 
+UDP_IP = "127.0.0.1"
+UDP_PORT = 9000
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
 class packet:
 	def __init__(self, c=0, l=0, s=0, d=[]):
 		self.chksum = c
@@ -20,12 +27,6 @@ class packet:
 			res = '0'.encode() + res
 		return res
 		
-UDP_IP = "127.0.0.1"
-UDP_PORT = 9000
-
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
