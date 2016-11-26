acb4b18aa54a522dc8fd094cb1d81743b1bbaed9
robert1826 <robert1826@gmail.com>
2016-11-26 17:17:26 +0200

    added packet class

--- client.py
+++ client.py
@@ -1,5 +1,25 @@
 import socket
 
+class packet:
+	def __init__(self, c=0, l=0, s=0, d=[]):
+		self.chksum = c
+		self.length = l
+		self.seqno = s
+		self.data = d[:]
+
+	def toBuffer():
+		res = self.int_to_bytes(chksum, 16)
+		res += self.int_to_bytes(length, 16)
+		res += self.int_to_bytes(seqno, 32)
+		res += self.data
+		return res
+
+	def int_to_bytes(n, length):
+		res = str(n).encode()
+		while len(res) < length:
+			res = '0'.encode() + res
+		return res
+
 if __name__ == "__main__":
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     #sock.connect(("localhost", 9000))
